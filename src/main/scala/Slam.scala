6d2a43373138cb0fe9d334c3f06ac8ff018fda5c
scmalte <scmalte@ethz.ch>
2010-11-11 14:53:33 +0100

    Changed SBT project, exploring the Scala AST

--- src/main/scala/Slam.scala
+++ src/main/scala/Slam.scala
@@ -2,10 +2,11 @@ package de.oakgrove.slam
 
 import scala.tools.nsc.{Global, Phase}
 import scala.tools.nsc.plugins.{Plugin, PluginComponent}
+// import com.weiglewilczek.slf4s.Logging
 
-class Slam(val global: Global) extends Plugin {
-  import global.{CompilationUnit, Traverser, ForeachTreeTraverser, Tree, Apply}
-  // import global.definitions._
+class Slam(val global: Global) extends Plugin { // with Logging {
+  import global.{CompilationUnit, Traverser, ForeachTreeTraverser, Tree}
+	import global.{Apply, ClassDef}
 	
 	val name = "slam"
 	val description = "Creates class diagrams"
@@ -27,33 +28,25 @@ class Slam(val global: Global) extends Plugin {
 			def apply(unit: CompilationUnit) {
 				(new ForeachTreeTraverser(handle)).traverse(unit.body)
 			}
-			
+
+/*
+(sym: Slam.this.global.Symbol,constrMods: Slam.this.global.Modifiers,vparamss: List[List[Slam.this.global.ValDef]],argss:List[List[Slam.this.global.Tree]],body: List[Slam.this.global.Tree],superPos: Slam.this.global.Position)Slam.this.global.ClassDef
+
+(sym: Slam.this.global.Symbol,impl: Slam.this.global.Template)Slam.this.global.ClassDef 
+
+object Slam.this.global.ClassDef
+*/
+
 			def handle(tree: Tree): Unit = tree match {
-				case Apply(fun, args) =>
-					println("traversing application of "+ fun)
-					
-				case _ =>
-					println(tree)
+				case cd @ ClassDef(mods, name, tparams, impl) => 
+					println("-- 1 --" + tree.productPrefix)
+					println("\t name = " + name)
+					println(impl.productPrefix)
+					// println("\t mods = " + mods)
+					// println("\t tparams = " + tparams)
+				
+				case _ => () //println(tree.productPrefix)
 			}
 		}
 	}
-
-
-  // def newPhase(prev: Phase): Phase = new TraverserPhase(prev)
-	
-  // class TraverserPhase(prev: Phase) extends StdPhase(prev) {
-    // def apply(unit: CompilationUnit) {
-      // newTraverser().traverse(unit.body)
-    // }
-  // }
-
-  // def newTraverser(): Traverser = new ForeachTreeTraverser(check)
-
-  // def check(tree: Tree): Unit = tree match {
-    // case Apply(fun, args) =>
-      // println("traversing application of "+ fun)
-			
-    // case _ =>
-			// println(tree)
-  // }
 }
